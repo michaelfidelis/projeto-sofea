eece8891560c328ba09eae2b20379345a8abf93b
Michael Fidelis <mclexr@gmail.com>
2016-11-16 00:56:20 -0200

    ajustes grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     grunt.registerTask('default', [
-       'uglify'
-        'cssmin']);
+       'uglify', 'cssmin']);
 
 };
