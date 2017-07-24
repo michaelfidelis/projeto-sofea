c152bca153077d16e0ab58380c7e4291c6e01ad5
Michael Fidelis <mclexr@gmail.com>
2017-07-23 22:45:03 -0300

    ajustes bower

--- modules/application/application.module.js
+++ modules/application/application.module.js
@@ -1,7 +1,6 @@
 (function () {
     'use strict';
     angular.module('projetosofea.application', [
-		'ngResource',
 		'ui.router',
 		'projetosofea.common',
 		'projetosofea.cadastros'
--- modules/common/service/common.service.module.js
+++ modules/common/service/common.service.module.js
@@ -1,4 +1,6 @@
 (function () {
     'use strict';
-    angular.module('projetosofea.common.service', []);
+    angular.module('projetosofea.common.service', [
+        'ngResource'
+    ]);
 })();
